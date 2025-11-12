8d611eb8cb901929997f9b7d3bca217ac42a9679
Felix Larose-Gervais <filedesless@gmail.com>
2025-11-12 01:05:27 -0500

    karatsuba impl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![feature(trait_alias)]
 mod poly;
 
+use std::time::Instant;
+
 use poly::Polynomial;
 
 fn main() {
-    let a = Polynomial::new(vec![2, 1]);
-    let b = Polynomial::new(vec![4, 3, 2]);
-    let c = &a * &b;
-    println!("Hello polynomial world: ({:?}) * ({:?}) = {:?}", a, b, c);
+    let a = Polynomial::new(Vec::from_iter(0..5));
+    let b = Polynomial::new(Vec::from_iter(0..5));
+
+    println!("# Dumb multiplication");
+    let now = Instant::now();
+    let c_schoolbook = &a.multiply_schoolbook(&b);
+    let elapsed = now.elapsed();
+    println!("   ({:?})\n x ({:?})\n = {:?}", a, b, c_schoolbook);
+    println!("Elapsed time: {:?}", elapsed);
+
+    println!();
+
+    println!("# Fancy schmancy multiplication");
+    let now = Instant::now();
+    let c_karatsuba = &a.multiply_karatsuba(&b);
+    let elapsed = now.elapsed();
+    println!("   ({:?})\n x ({:?})\n = {:?}", a, b, c_karatsuba);
+    println!("Elapsed time: {:?}", elapsed);
+
+    assert_eq!(c_karatsuba, c_schoolbook);
 }
--- src/poly.rs
+++ src/poly.rs
@@ -1,10 +1,13 @@
-use std::{fmt, ops::Mul};
+use std::{
+    fmt,
+    ops::{Add, Mul, Sub},
+};
 
 use num::Num;
 
 pub trait Ring = Num + Copy + Clone + fmt::Debug;
 
-/// Polynomial over a ring R represented by its vector of coefficients.
+/// Polynomial over a ring R represented by its vector of coefficients (lowest degree first).
 #[derive(Clone)]
 pub(crate) struct Polynomial<R: Ring>(Vec<R>);
 
@@ -19,7 +22,7 @@ impl<R: Ring> Polynomial<R> {
         self.0.len() - 1
     }
 
-    fn multiply(&self, other: &Polynomial<R>) -> Polynomial<R> {
+    pub fn multiply_schoolbook(&self, other: &Polynomial<R>) -> Polynomial<R> {
         let mut result = vec![R::zero(); self.degree() + other.degree() + 1];
         for (i, &ai) in self.0.iter().enumerate() {
             for (j, &bj) in other.0.iter().enumerate() {
@@ -28,26 +31,104 @@ impl<R: Ring> Polynomial<R> {
         }
         Polynomial(result)
     }
+
+    pub fn multiply_karatsuba(&self, other: &Polynomial<R>) -> Polynomial<R> {
+        let n = self.0.len().max(other.0.len()).next_power_of_two();
+        let mut a = self.clone();
+        let mut b = other.clone();
+        a.0.resize(n, R::zero());
+        b.0.resize(n, R::zero());
+        a.multiply_karatsuba_rec(&b)
+    }
+
+    fn multiply_karatsuba_rec(&self, other: &Polynomial<R>) -> Polynomial<R> {
+        let n = self.0.len();
+        assert_eq!(n, other.0.len());
+        assert!(n.is_power_of_two());
+        // base case - TODO: find a good threshold for schoolbook fallback
+        if n == 1 {
+            return self.multiply_schoolbook(other);
+        }
+        let m = n / 2;
+
+        // TODO: maybe try to reduce the crazy heap allocations
+        let x0 = Polynomial::from(&self.0[..m]);
+        let x1 = Polynomial::from(&self.0[m..]);
+        let y0 = Polynomial::from(&other.0[..m]);
+        let y1 = Polynomial::from(&other.0[m..]);
+
+        let z0 = x0.multiply_karatsuba_rec(&y0);
+        let z2 = x1.multiply_karatsuba_rec(&y1);
+        let z1 = &(&(&x0 + &x1).multiply_karatsuba_rec(&(&y0 + &y1)) - &z0) - &z2;
+
+        let z1 = z1.multiply_by_monic_monomial(m);
+        let z2 = z2.multiply_by_monic_monomial(n);
+
+        &(&z0 + &z1) + &z2
+    }
+
+    pub fn multiply_by_monomial(&self, degree: usize, coefficient: R) -> Polynomial<R> {
+        let zeros = vec![R::zero(); degree];
+        let result = Vec::from_iter(
+            zeros
+                .into_iter()
+                .chain(self.0.iter().map(|&coeff| coeff * coefficient)),
+        );
+        Polynomial::from(result)
+    }
+
+    pub fn multiply_by_monic_monomial(&self, degree: usize) -> Polynomial<R> {
+        self.multiply_by_monomial(degree, R::one())
+    }
 }
 
 impl<R: Ring> fmt::Debug for Polynomial<R> {
     /// How to display a polynomial (higher degree coefficient first)
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (i, &coeff) in self.0.iter().enumerate().rev() {
-            if !coeff.is_zero() {
-                if i == 0 {
-                    write!(f, "{:?}", coeff)?;
-                } else if i == 1 {
-                    write!(f, "{:?}x", coeff)?;
-                } else {
-                    write!(f, "{:?}x^{}", coeff, i)?;
-                }
-                if i != 0 {
-                    write!(f, " + ")?;
-                }
-            }
+        let format_string = self
+            .0
+            .iter()
+            .enumerate()
+            .rev()
+            .filter(|(_, coeff)| !coeff.is_zero())
+            .map(|(i, &coeff)| match i {
+                0 => format!("{coeff:?}"),
+                1 => format!("{coeff:?}x"),
+                _ => format!("{coeff:?}x^{i}"),
+            })
+            .collect::<Vec<String>>()
+            .join(" + ");
+        write!(f, "{}", format_string)
+    }
+}
+
+impl<R: Ring> Add for &Polynomial<R> {
+    type Output = Polynomial<R>;
+
+    fn add(self, other: &Polynomial<R>) -> Self::Output {
+        let mut result = vec![R::zero(); self.degree().max(other.degree()) + 1];
+        for (i, &ai) in self.0.iter().enumerate() {
+            result[i] = result[i] + ai;
         }
-        Ok(())
+        for (i, &bi) in other.0.iter().enumerate() {
+            result[i] = result[i] + bi;
+        }
+        Polynomial(result)
+    }
+}
+
+impl<R: Ring> Sub for &Polynomial<R> {
+    type Output = Polynomial<R>;
+
+    fn sub(self, other: &Polynomial<R>) -> Self::Output {
+        let mut result = vec![R::zero(); self.degree().max(other.degree()) + 1];
+        for (i, &ai) in self.0.iter().enumerate() {
+            result[i] = result[i] + ai;
+        }
+        for (i, &bi) in other.0.iter().enumerate() {
+            result[i] = result[i] - bi;
+        }
+        Polynomial(result)
     }
 }
 
@@ -55,6 +136,28 @@ impl<R: Ring> Mul for &Polynomial<R> {
     type Output = Polynomial<R>;
 
     fn mul(self, other: &Polynomial<R>) -> Self::Output {
-        self.multiply(&other)
+        // self.multiply_schoolbook(&other)
+        self.multiply_karatsuba(&other)
+    }
+}
+
+impl<R: Ring> From<&[R]> for Polynomial<R> {
+    fn from(coefficients: &[R]) -> Self {
+        Self(coefficients.to_vec())
+    }
+}
+
+impl<R: Ring> From<Vec<R>> for Polynomial<R> {
+    fn from(coefficients: Vec<R>) -> Self {
+        Self(coefficients)
+    }
+}
+
+impl<R: Ring> PartialEq for Polynomial<R> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0
+            .iter()
+            .zip(other.0.iter())
+            .all(|(&a, &b)| (a == R::zero() && b == R::zero()) || a == b)
     }
 }
